779780c85ed0436f11b020ff94addb2276096e67
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-10-16 18:23:07 -0400

    Updated code for Exercise 6.3

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,7 +8,7 @@ const apiUrl = 'https://movie-api-cf.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   //Inject the HttpClient module to the contructor params
   //This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  ngOnInit(): void { }
+
+  /**
+   * on login token, userdata, and Username will be stored in localstorage. 
+   * user will be sent to the movie page 
+   */
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((data) => {
+      
+      localStorage.setItem("user", JSON.stringify(data.user))
+      localStorage.setItem("token", data.token);
+      localStorage.setItem('Username', data.user.Username)
+
+      this.dialogRef.close();
+      console.log(data);
+      this.snackBar.open('you\'ve been logged in', 'OK', {
+        duration: 2000
+      });
+    }, (data) => {
+      console.log(data);
+      this.snackBar.open('sorry, something went wrong. please try again', 'OK', {
+        duration: 2000
+      });
+    })
+  }
+}
